7ebe66086f7d23fffd5ae1014260f31d3f1df2bb
XylophoneHero <nick97000@gmail.com>
2021-07-18 18:00:10 +0100

    fix blog from json

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,6 +1,6 @@
 import DynamicComponent from "@/components/DynamicComponent"
 
-const BlogIndex = ({ data, allBlogs }) =>
+const BlogIndex = ({ data }) =>
 {
   return data.sections.map((section, index) => <DynamicComponent section={section} key={section.__component + section.id} sectionId={`section-${index}`} />)
 
@@ -10,17 +10,19 @@ export default BlogIndex
 
 export async function getStaticProps()
 {
-  const allBlogsRes = await fetch("http://localhost:1337/blogs")
-  const allBlogs = await allBlogsRes.json()
-
-  const pageRes = await fetch("http://localhost:1337/pages?slug=blog")
-  const data = await pageRes.json()
-
-  return {
-    props: {
-      data: data[0],
-      allBlogs,
-    }
+  // const allBlogsRes = await fetch("http://localhost:1337/blogs")
+  // const allBlogs = await allBlogsRes.json()
+  let data
+  if (process.env.NODE_ENV === "development")
+  {
+    const pageRes = await fetch("http://localhost:1337/pages?slug=blog")
+    data = await pageRes.json()
+  } else
+  {
+    // const res = await fetch("https://raw.githubusercontent.com/xylophonehero/nestor-holdings/main/public/pages.json?token=AQOM4DWHRN2ZZSGLLLRKTH3A5NHRM")
+    data = pages.filter((page) => page.slug === "blog")
   }
-
+  return {
+    props: { data: data[0] },
+  }
 }
