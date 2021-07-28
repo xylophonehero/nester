4706ee460294f2596ca4800fab477989577f987a
XylophoneHero <nick97000@gmail.com>
2021-07-28 17:54:43 +0100

    outlines important

--- components/sections/ImageText.js
+++ components/sections/ImageText.js
@@ -21,7 +21,7 @@ const ImageText = ({ data, sectionId }) =>
         </div>
       </div>
       <div tw="flex-1 relative w-full padding-top[95%] tablet:padding-top[60%] laptop:(min-height[50vh] pt-0) desktop:(min-height[75vh])">
-        <StrapiImage image={data.image} layout="fill" objectFit="cover" />
+        <StrapiImage image={data.image} layout="fill" objectFit="cover" quality={50} />
         {/* <Image src={data.image.name} alt={data.image.alternativeText} layout="fill" objectFit="cover" /> */}
       </div>
     </Wrapper>
