4e21d61c8b0786b7476d99688882c8f9533bfe26
XylophoneHero <nick97000@gmail.com>
2021-08-01 14:15:03 +0100

    test ios not going to top

--- pages/_app.js
+++ pages/_app.js
@@ -4,9 +4,31 @@ import GlobalStyles from '@/components/layout/GlobalStyles'
 import { GlobalStyles as MacroStyles } from "twin.macro"
 
 import UserContextProvider from 'context/UserContext'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps })
 {
+  const slug = pageProps.data.slug
+
+  useEffect(() =>
+  {
+    const scrollToTop = () =>
+    {
+      window.scroll({
+        top: 0,
+        left: 0,
+        behavior: 'auto',
+      })
+    }
+    const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream
+    if (isIOS)
+    {
+      setTimeout(() =>
+      {
+        scrollToTop()
+      }, 5)
+    }
+  }, [slug])
 
   return <UserContextProvider>
     <Layout>
