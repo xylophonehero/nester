bef8baee3d03a741b63d56d537e7ad068d9aa9d2
XylophoneHero <nick97000@gmail.com>
2021-08-31 17:40:29 +0100

    next router test

--- pages/react.js
+++ pages/react.js
@@ -1,9 +1,12 @@
+import Link from 'next/link';
+import {useRouter} from "next/router"
 import ReactRouteUpdate from '@/components/general/ReactRouteUpdate';
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 export default function App()
 {
+  const router = useRouter()
   return (
     <div suppressHydrationWarning>
       {typeof window === 'undefined' ? null :
@@ -13,6 +16,8 @@ export default function App()
             <Switch>
               <Route path="/about">
                 <h2>About</h2>
+                <Link href="/">home</Link>
+                <button onClick={() => router.push("/")}>Home</button>
               </Route>
               <Route path="/test">
                 <h2>Test</h2>
